5e492e58d9222d8de75a4365069d068b73d3d3c8
Jeffrey Whitaker <jeffrey.david.whitaker@zoho.com>
2019-08-22 13:43:42 -0700

    got form working, axios post, working on print to screen

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React from 'react'
-import './App.css'
 import FormMaker from './Components/FormMaker'
 
 function App() {
--- src/Components/FormMaker.js
+++ src/Components/FormMaker.js
@@ -2,20 +2,49 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios'
 import { Form, Field, withFormik } from 'formik'
 import * as Yup from 'yup'
+import '../index.css'
 
 const FormMaker = ({errors, touched, values, status }) => {
+    const[users, setUsers] = useState([])
+    console.log('this is touched', touched)
+    useEffect(() => {
+        if (status) {
+            setUsers([...users, status])
+        }
+    }, [status])
+
     return (
+        <>
         <Form className="formContainer">
             <Field type='text' name='personName' placeholder='Name' />
+            {touched.personName && errors.personName && (
+                <p className='error'>{errors.personName}</p>
+            )}
+
             <Field type='email' name='personEmail' placeholder='example@email.com' />
+            {touched.personEmail && errors.personEmail && (
+                <p className='error'>{errors.personEmail}</p>
+            )}
+
             <Field type='password' name='personPassword' placeholder='password' />
+            {touched.personPassword && errors.personPassword && (
+                <p className='error'>{errors.personPassword}</p>
+            )}
+
             <label className='checkboxContainer'>
                 Terms of Service
-                <Field name='personTOS' type='checkbox' checked={''} />
+                <Field name='personTOS' type='checkbox' checked={values.personTOS} />
                 <span className='checkmark' />
+                {touched.personTOS && errors.personTOS && (
+                    <p className='error'>{errors.personTOS}</p>
+                )}
             </label>
             <button type='submit'>Submit</button>
         </Form>
+        <div className='usersContainer'>
+            <p>{users}</p>
+        </div>
+        </>
     )
 }
 
@@ -27,6 +56,21 @@ const FormikFormMaker = withFormik({
             personPassword: personPassword || '',
             personTOS: personTOS || false,
         }
+    },
+
+    validationSchema: Yup.object().shape({
+        personName: Yup.string().required("** Name is a required field"),
+        personEmail: Yup.string().required("** Email is a required field"),
+        personPassword: Yup.string().required("** Password is a required field"),
+        personTOS: Yup.boolean().oneOf([true], "** Must accept Terms and Conditions")
+    }),
+
+    handleSubmit(values, { setStatus }) {
+        axios.post('https://reqres.in/api/users/', values)
+            .then(res => {
+                setStatus(res.data)
+            })
+            .catch(err => console.log(err.response))
     }
 })(FormMaker)
 
